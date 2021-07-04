863085265e620727f2349b02349feb3b793f2703
Artic030 <030ci@mail.ru>
2021-07-05 01:46:23 +0300

    Add electric ic2 EnderCannon

---
+++ src/main/java/ru/artic030/mod02/items/EnderCannon.java
@@ -0,0 +1,84 @@
+package ru.artic030.mod02.items;
+
+import ic2.api.item.ElectricItem;
+import ic2.api.item.IElectricItem;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.item.EntityEnderPearl;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntitySnowball;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import ru.artic030.mod02.Mod02;
+import ru.artic030.mod02.load.ItemLoader;
+import ru.artic030.mod02.utils.IHasModel;
+
+public class EnderCannon extends Item implements IHasModel, IElectricItem {
+	
+	public static int maxCharge = 64000;
+	public static byte tier = 1;
+	public static short TransferLimit = 128;
+	public static short preUse = 1200;
+
+	public EnderCannon(String name) {
+		this.setUnlocalizedName(name);
+		this.setRegistryName(name);
+		this.setCreativeTab(CreativeTabs.REDSTONE);
+		this.setMaxDamage(27);
+		ItemLoader.ITEMS.add(this);
+	}
+	
+	@Override
+	public boolean canProvideEnergy(ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public double getMaxCharge(ItemStack stack) {
+		return this.maxCharge;
+	}
+
+	@Override
+	public int getTier(ItemStack stack) {
+		return this.tier;
+	}
+
+	@Override
+	public double getTransferLimit(ItemStack stack) {
+		return this.TransferLimit;
+	}
+
+	@Override
+	public void registerModels() {
+		Mod02.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+	
+	public double getDurabilityForDisplay(ItemStack stack) {
+		return ((double)this.maxCharge  - (double)ElectricItem.manager.getCharge(stack)) / (double) this.maxCharge;
+	}
+	
+	public boolean showDurabilityBar(ItemStack stack, World world, EntityPlayer player) {
+		if(player.isCreative())
+			return false;
+		else return true;
+	}
+	
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		ItemStack stack = player.getHeldItem(hand);
+		if(world.isRemote)
+			return new ActionResult<ItemStack>(EnumActionResult.PASS, stack);
+		if(ElectricItem.manager.getCharge(stack) >= this.preUse) {
+			ElectricItem.manager.discharge(stack, --this.preUse, tier, true, false, false);
+		}
+		player.spawnSweepParticles();
+		if(!world.isRemote) {
+			EntityEnderPearl entitysnowball = new EntityEnderPearl(world, player);
+			entitysnowball.shoot(player, player.rotationPitch, player.rotationYaw, 0.2F, 1.5F, 1.0F);
+			world.spawnEntity(entitysnowball);
+		}
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);			
+	}
+}
--- src/main/java/ru/artic030/mod02/load/ItemLoader.java
+++ src/main/java/ru/artic030/mod02/load/ItemLoader.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.item.Item;
+import ru.artic030.mod02.items.EnderCannon;
 import ru.artic030.mod02.items.FreezerCannon;
 import ru.artic030.mod02.items.ItemIronTreetap;
 import ru.artic030.mod02.items.ItemSteelWrench;
@@ -12,6 +13,7 @@ public class ItemLoader {
 
 		public static final List <Item> ITEMS = new ArrayList<Item>();
 		public static final Item FREEZE_CANNON = new FreezerCannon("freezer_cannon");
+		public static final Item ENDER_CANNON = new EnderCannon("ender_cannon");
 		public static final Item IRON_TREETAP = new ItemIronTreetap("iron_treetap");
 		public static final Item STEEL_WRENCH = new ItemSteelWrench("steel_wrench");	
 }
