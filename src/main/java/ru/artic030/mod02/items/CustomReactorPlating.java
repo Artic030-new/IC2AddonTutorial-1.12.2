3f8f893a976ed3a07df28ebf595d8350405f3cd9
Artic030 <030ci@mail.ru>
2021-07-28 20:41:07 +0300

    dfgfdgfd

---
+++ src/main/java/ru/artic030/mod02/items/CustomReactorPlating.java
@@ -0,0 +1,59 @@
+package ru.artic030.mod02.items;
+
+import ic2.api.reactor.IReactor;
+import ic2.api.reactor.IReactorComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class CustomReactorPlating extends Item implements IReactorComponent {
+
+	@Override
+	public boolean canBePlacedIn(ItemStack stack, IReactor reactor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void processChamber(ItemStack stack, IReactor reactor, int x, int y, boolean heatrun) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean acceptUraniumPulse(ItemStack stack, IReactor reactor, ItemStack pulsingStack, int youX, int youY,
+			int pulseX, int pulseY, boolean heatrun) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canStoreHeat(ItemStack stack, IReactor reactor, int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getMaxHeat(ItemStack stack, IReactor reactor, int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCurrentHeat(ItemStack stack, IReactor reactor, int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int alterHeat(ItemStack stack, IReactor reactor, int x, int y, int heat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float influenceExplosion(ItemStack stack, IReactor reactor) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
