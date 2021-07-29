fe136ee11868751ffaf4bd1e1bcbb5082e1560b6
Artic030 <030ci@mail.ru>
2021-07-29 20:33:53 +0300

    Custom reactor component

--- src/main/java/ru/artic030/mod02/items/CustomReactorPlating.java
+++ src/main/java/ru/artic030/mod02/items/CustomReactorPlating.java
@@ -2,58 +2,30 @@ package ru.artic030.mod02.items;
 
 import ic2.api.reactor.IReactor;
 import ic2.api.reactor.IReactorComponent;
+import ic2.core.item.reactor.AbstractReactorComponent;
+import ic2.core.ref.ItemName;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-public class CustomReactorPlating extends Item implements IReactorComponent {
+public class CustomReactorPlating extends AbstractReactorComponent {
+	   private final int maxHeatAdd;
+	   private final float effectModifier;
 
-	@Override
-	public boolean canBePlacedIn(ItemStack stack, IReactor reactor) {
-		// TODO Auto-generated method stub
-		return false;
+	   public CustomReactorPlating(ItemName name, int maxheatadd, float effectmodifier) {
+	      super(name);
+	      this.maxHeatAdd = 10000;
+	      this.effectModifier = 0.33F;
+	   }
+
+	   public void processChamber(ItemStack stack, IReactor reactor, int x, int y, boolean heatrun) {
+	      if (heatrun) {
+	         reactor.setMaxHeat(reactor.getMaxHeat() + this.maxHeatAdd);
+	         reactor.setHeatEffectModifier(reactor.getHeatEffectModifier() * this.effectModifier);
+	      }
+
+	   }
+
+	   public float influenceExplosion(ItemStack stack, IReactor reactor) {
+	      return this.effectModifier >= 1.0F ? 0.0F : this.effectModifier;
+	   }
 	}
-
-	@Override
-	public void processChamber(ItemStack stack, IReactor reactor, int x, int y, boolean heatrun) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean acceptUraniumPulse(ItemStack stack, IReactor reactor, ItemStack pulsingStack, int youX, int youY,
-			int pulseX, int pulseY, boolean heatrun) {
-		// TODO Auto-generated method stu
-		return false;
-	} 
-
-	@Override
-	public boolean canStoreHeat(ItemStack stack, IReactor reactor, int x, int y) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int getMaxHeat(ItemStack stack, IReactor reactor, int x, int y) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getCurrentHeat(ItemStack stack, IReactor reactor, int x, int y) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int alterHeat(ItemStack stack, IReactor reactor, int x, int y, int heat) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public float influenceExplosion(ItemStack stack, IReactor reactor) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-}
