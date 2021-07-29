d2b455e63b2d7dd1f29b7bfa31aa1a19c2271992
Artic030 <030ci@mail.ru>
2021-07-29 21:32:29 +0300

    thygthyty

--- src/main/java/ru/artic030/mod02/items/IngineerBattery.java
+++ src/main/java/ru/artic030/mod02/items/IngineerBattery.java
@@ -1,30 +1,57 @@
 package ru.artic030.mod02.items;
 
+import ic2.api.item.ElectricItem;
 import ic2.api.item.IElectricItem;
+import ic2.core.IC2;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import ru.artic030.mod02.load.ItemLoader;
 
-public class IngineerBattery extends Item implements IElectricItem{
+public class IngineerBattery extends Item implements IElectricItem {
 
+	private int maxCharge = 60000;
+	private byte tier = 2;
+	private int transfer = 10;
+	
+	public IngineerBattery(String name) {
+		this.setUnlocalizedName(name);
+		this.setRegistryName(name);
+		this.setCreativeTab(IC2.tabIC2);
+		this.setMaxDamage(27);
+		ItemLoader.ITEMS.add(this);
+	}
+	
+	public double getDurabilityForDisplay(ItemStack stack) {
+		return ((double)this.maxCharge  - (double)ElectricItem.manager.getCharge(stack)) / (double) this.maxCharge;
+	}
+	
+	public boolean showDurabilityBar(ItemStack stack, World world, EntityPlayer player) {
+		if(player.isCreative())
+			return false;
+		else return true;
+	}
+	
 	@Override
 	public boolean canProvideEnergy(ItemStack stack) {
-		return false;
+		return true;
 	}
 
 	@Override
 	public double getMaxCharge(ItemStack stack) {
 		
-		return 60000;
+		return maxCharge;
 	}
 
 	@Override
 	public int getTier(ItemStack stack) {
-		return 2;
+		return tier;
 	}
 
 	@Override
 	public double getTransferLimit(ItemStack stack) {
-		return 10;
+		return transfer;
 	}
 
 }
