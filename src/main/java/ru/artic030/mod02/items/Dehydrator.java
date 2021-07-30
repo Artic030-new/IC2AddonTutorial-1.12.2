bb9d360322883267dee1f7c2832902db2cda8a16
Artic030 <030ci@mail.ru>
2021-07-30 22:47:44 +0300

    Edit dehydrator

--- src/main/java/ru/artic030/mod02/items/Dehydrator.java
+++ src/main/java/ru/artic030/mod02/items/Dehydrator.java
@@ -2,6 +2,8 @@ package ru.artic030.mod02.items;
 
 
 import java.util.EnumSet;
+import java.util.IdentityHashMap;
+import java.util.Map;
 
 import ic2.api.item.ElectricItem;
 import ic2.core.IC2;
@@ -11,11 +13,16 @@ import ic2.core.item.tool.HarvestLevel;
 import ic2.core.item.tool.ItemElectricTool;
 import ic2.core.item.tool.ToolClass;
 import ic2.core.ref.ItemName;
+import ic2.core.util.StackUtil;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Enchantments;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -29,7 +36,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class Dehydrator extends ItemElectricTool {
 	
-	public int dehydrationEnergyCost = 8888;
+	private int dehydrationEnergyCost = 8888;
+	private boolean isCombatModeOn = false;
 	
 	public Dehydrator() {
 		super((ItemName)null, 1200, HarvestLevel.Stone, EnumSet.of(ToolClass.Shovel, ToolClass.Sword));
@@ -37,7 +45,7 @@ public class Dehydrator extends ItemElectricTool {
 		this.maxCharge = 120000;
 		this.transferLimit = 20000;
 		this.tier = 2;
-		this.attackDamage = 12.0F;
+		this.attackDamage = 2.0F;
 	}
 	
 	@SideOnly(Side.CLIENT)
@@ -48,7 +56,7 @@ public class Dehydrator extends ItemElectricTool {
 	public String locationReplacer() {
 		return "mod02." + super.getUnlocalizedName().substring(4);
 	}
-	
+	@Override
 	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitx, float hitY, float hitZ) {
 		IBlockState state = world.getBlockState(pos);
 		ItemStack stack = player.getHeldItem(hand);
@@ -77,5 +85,22 @@ public class Dehydrator extends ItemElectricTool {
 		player.sendMessage(new TextComponentString("Внимание! Не хватает энергии для осуществления операции."));
 			return EnumActionResult.FAIL;
 	}
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		if((!world.isRemote) && (IC2.keyboard.isModeSwitchKeyDown(player))) {	
+			if(!isCombatModeOn) {
+				isCombatModeOn = true;
+				this.attackDamage = 12.0F;
+				
+				IC2.platform.messagePlayer(player, "Боевой режим активирован");
+			} else {
+				IC2.platform.messagePlayer(player, "Боевой режим деактивирован");
+				isCombatModeOn = false;
+				this.attackDamage = 2.0F;
+				return super.onItemRightClick(world, player, hand);
+			}
+		}
+			return super.onItemRightClick(world, player, hand);
+}
 
 }
