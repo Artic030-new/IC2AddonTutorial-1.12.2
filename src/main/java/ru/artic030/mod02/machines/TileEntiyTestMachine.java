51418182bc06cf9a2f320d5898b88d04b2cfdd6a
Artic030 <030ci@mail.ru>
2021-08-01 10:15:37 +0300

    dfghfdgdfghg

--- src/main/java/ru/artic030/mod02/machines/TileEntiyTestMachine.java
+++ src/main/java/ru/artic030/mod02/machines/TileEntiyTestMachine.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import ic2.api.recipe.IMachineRecipeManager;
 import ic2.api.recipe.IRecipeInput;
+import ic2.api.recipe.MachineRecipeResult;
 import ic2.api.upgrade.IUpgradableBlock;
 import ic2.api.upgrade.UpgradableProperty;
 import ic2.core.ContainerBase;
@@ -30,15 +31,15 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class TileEntiyTestMachine extends TileEntityElectricMachine implements IHasGui, IGuiValueProvider, IUpgradableBlock {
 	
-	public final int idleEU;
-	public final int activeEU;
-	public final int maxProgress;
+	protected final int idleEU;
+	protected final int activeEU;
+	protected final int maxProgress;
 	
 	public final InvSlotProcessable<IRecipeInput, Collection<ItemStack>, ItemStack> inputSlot;
 	public final InvSlotOutput outputSlot;
 	public final InvSlotUpgrade upgradeSlot;
 	
-	public final Redstone redstone;
+	protected final Redstone redstone;
 	
 	@GuiSynced public int progress;
 	
@@ -58,6 +59,99 @@ public class TileEntiyTestMachine extends TileEntityElectricMachine implements I
 		this.redstone = (Redstone)this.addComponent(new Redstone(this));
 
 	   }
+	
+	protected boolean canRun() {
+	        return true;
+	    }
+	
+	protected void updateEntityServer() {
+	      super.updateEntityServer();
+	      boolean needsInvUpdate = false;
+	      boolean canOperate = this.canOperate();
+	      if (this.progress >= this.maxProgress) {
+	         while(true) {
+	            if (this.progress < this.maxProgress || !canOperate) {
+	               needsInvUpdate = true;
+	               break;
+	            }
+
+	            this.operate();
+	            this.progress -= this.maxProgress;
+	            canOperate = this.canOperate();
+	         }
+	      }
+
+	      boolean spinUp;
+	      if (this.canRun()) {
+	         if (canOperate && this.energy.useEnergy((double)this.activeEU)) {
+	            spinUp = true;
+	            this.progress += this.heat;
+	            this.updateSound(0);
+	         } else {
+	            spinUp = this.redstone.hasRedstoneInput();
+	            this.progress = 0;
+	            if (spinUp && !this.energy.useEnergy((double)this.idleEU)) {
+	               spinUp = false;
+	            } else {
+	               this.updateSound(2);
+	            }
+	         }
+	      } else {
+	         spinUp = false;
+	      }
+
+	      if (spinUp) {
+	         this.heat = (short)Math.min(10000, this.heat + 1);
+	      } else {
+	         this.heat = (short)Math.max(0, this.heat - 2);
+	         if (this.heat <= 0) {
+	            if (this.getActive()) {
+	               this.updateSound(1);
+	            }
+	         } else {
+	            this.updateSound(2);
+	         }
+	      }
+
+	      Iterator var4 = this.upgradeSlot.iterator();
+
+	      while(var4.hasNext()) {
+	         ItemStack stack = (ItemStack)var4.next();
+	         if (!StackUtil.isEmpty(stack) && stack.func_77973_b() instanceof IUpgradeItem) {
+	            needsInvUpdate |= ((IUpgradeItem)stack.func_77973_b()).onTick(stack, this);
+	         }
+	      }
+
+	      this.setActive(this.heat > 0);
+	      if (needsInvUpdate) {
+	         this.func_70296_d();
+	      }
+
+	   }
+	
+	public boolean canOperate() {
+        if (this.inputSlot.isEmpty()) {
+            return false;
+        }
+        final MachineRecipeResult<IRecipeInput, Collection<ItemStack>, ItemStack> output = (MachineRecipeResult<IRecipeInput, Collection<ItemStack>, ItemStack>)this.inputSlot.process();
+        return output != null && this.outputSlot.canAdd((Collection)output.getOutput());
+    }
+    
+    public void operate() {
+        assert this.canOperate();
+        final MachineRecipeResult<IRecipeInput, Collection<ItemStack>, ItemStack> output = (MachineRecipeResult<IRecipeInput, Collection<ItemStack>, ItemStack>)this.inputSlot.process();
+        this.processUpgrades((Collection<ItemStack>)output.getOutput());
+        this.outputSlot.add((Collection)output.getOutput());
+        this.inputSlot.consume((MachineRecipeResult)output);
+    }
+    
+    protected void processUpgrades(final Collection<ItemStack> output) {
+        for (final ItemStack stack : this.upgradeSlot) {
+            if (stack != null && stack.func_77973_b() instanceof IUpgradeItem) {
+                ((IUpgradeItem)stack.func_77973_b()).onProcessEnd(stack, (IUpgradableBlock)this, (Collection)output);
+            }
+        }
+    }
 	
 	@Override
 	public void readFromNBT(final NBTTagCompound nbt) {
