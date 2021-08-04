af6b4ecc7ec70e8daa918f3ca01403f7a2c1eba7
Artic030 <030ci@mail.ru>
2021-08-04 21:23:26 +0300

    fdsdf

--- src/main/java/ru/artic030/mod02/armor/DigitalArmor.java
+++ src/main/java/ru/artic030/mod02/armor/DigitalArmor.java
@@ -1,14 +1,33 @@
 package ru.artic030.mod02.armor;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import ic2.api.item.ElectricItem;
 import ic2.api.item.HudMode;
 import ic2.api.item.IItemHudProvider;
+import ic2.core.IC2;
+import ic2.core.init.Localization;
+import ic2.core.item.ItemTinCan;
 import ic2.core.item.armor.ItemArmorElectric;
 import ic2.core.item.armor.jetpack.IJetpack;
 import ic2.core.ref.ItemName;
+import ic2.core.util.StackUtil;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class DigitalArmor extends ItemArmorElectric implements IJetpack, IItemHudProvider {
 
@@ -17,8 +36,13 @@ public class DigitalArmor extends ItemArmorElectric implements IJetpack, IItemHu
 	}
 	
 	public boolean addsProtection(EntityLivingBase entity, EntityEquipmentSlot slot, ItemStack stack) {
-	      return ElectricItem.manager.getCharge(stack) > 0.0D;
-	   }
+		return ElectricItem.manager.getCharge(stack) > 0.0D;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public EnumRarity getRarity(ItemStack stack) {
+		return EnumRarity.RARE;
+	}
 
 	@Override
 	public boolean doesProvideHUD(ItemStack stack) {
@@ -74,5 +98,34 @@ public class DigitalArmor extends ItemArmorElectric implements IJetpack, IItemHu
 	public int getEnergyPerDamage() {
 		return 0;
 	}
+	
+	public int getItemEnchantability() {
+		return 0;
+	}
+	
+	public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
+	      boolean ret = false;
+	      switch(this.armorType) {
+	      case HEAD:
+	         if (ElectricItem.manager.canUse(stack, 1000.0D)) {
+	            player.addPotionEffect(new PotionEffect(Potion.getPotionById(19), 100, 0));
+	            ElectricItem.manager.use(stack, 1000.0D, (EntityLivingBase)null);
+	            ret = true;
+	         } 
+	         break;
+	      case CHEST:
+	         break;
+	      case LEGS:
+	         break;
+	      case FEET:
+	    	  break;
+		default:
+			break;
+	      }
 
+	      if (ret) {
+	         player.inventoryContainer.detectAndSendChanges();
+	      }
+
+	   }
 }
